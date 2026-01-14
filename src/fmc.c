a5aa8612fe3ed2b762ad4a03cdf491df8fcfdea4
Jakob Kastelic <jkastelic@thinksrs.com>
2026-01-14 09:58:08 -0800

    cleanup of fmc code

--- src/fmc.c
+++ src/fmc.c
@@ -20,25 +20,6 @@
 #include <inttypes.h>
 #include <stdint.h>
 
-// TODO: remove this
-/* Device name: MT29F8G08ABACA */
-#define BSP_NAND_MANUFACTURER_CODE  0x2C
-#define BSP_NAND_DEVICE_CODE        0xD3
-#define BSP_NAND_THIRD_ID           0x90
-#define BSP_NAND_FOURTH_ID          0xA6
-#define BSP_NAND_BLOCK_NBR          4096   /* Blocks */
-#define BSP_NAND_BLOCK_SIZE         64     /* Pages  */
-#define BSP_NAND_PLANE_SIZE         2048   /* Blocks */
-#define BSP_NAND_PAGE_SIZE          4096   /* Bytes  */
-#define BSP_NAND_PLANE_NBR          2      /* Planes */
-#define BSP_NAND_SPARE_AREA_SIZE    224    /* Bytes  */
-#define BSP_NAND_ECC_SECTOR_SIZE    512
-#define BSP_NAND_ECC_ALGO           FMC_NAND_ECC_ALGO_BCH
-#define BSP_NAND_ECC_BCH_MODE       FMC_NAND_BCH_8BIT
-#define BSP_NAND_BAD_BLOCK_OFFSET   0
-#define BSP_NAND_ECC_OFFSET         2
-#define BSP_NAND_EXTRA_OFFSET       (BSP_NAND_ECC_OFFSET + BSP_NAND_ECC_SIZE_PER_SECTOR * (BSP_NAND_PAGE_SIZE / BSP_NAND_ECC_SECTOR_SIZE))
-
 // global variables
 static NAND_HandleTypeDef hnand;
 
@@ -94,14 +75,6 @@ void fmc_init(int argc, uint32_t arg1, uint32_t arg2, uint32_t arg3)
    SetupGPIO(GPIOE, &GPIO_Init_Structure, GPIO_PIN_8); /* FMC_D5: PE8 */
    SetupGPIO(GPIOE, &GPIO_Init_Structure, GPIO_PIN_9); /* FMC_D6: PE9 */
    SetupGPIO(GPIOE, &GPIO_Init_Structure, GPIO_PIN_10); /* FMC_D7: PE10 */
-//   SetupGPIO(GPIOE, &GPIO_Init_Structure, GPIO_PIN_11); /* FMC_D8: PE11 */
-//   SetupGPIO(GPIOE, &GPIO_Init_Structure, GPIO_PIN_12); /* FMC_D9: PE12 */
-//   SetupGPIO(GPIOE, &GPIO_Init_Structure, GPIO_PIN_13); /* FMC_D10: PE13 */
-//   SetupGPIO(GPIOE, &GPIO_Init_Structure, GPIO_PIN_14); /* FMC_D11: PE14 */
-//   SetupGPIO(GPIOE, &GPIO_Init_Structure, GPIO_PIN_15); /* FMC_D12: PE15 */
-//   SetupGPIO(GPIOB, &GPIO_Init_Structure, GPIO_PIN_8); /* FMC_D13: PB8 */
-//   SetupGPIO(GPIOD, &GPIO_Init_Structure, GPIO_PIN_9); /* FMC_D14: PD9 */
-//   SetupGPIO(GPIOD, &GPIO_Init_Structure, GPIO_PIN_10); /* FMC_D15: PD10 */
 
    /* Enable and set interrupt to the lowest priority */
    IRQ_SetPriority(FMC_IRQn, 0x0F);
@@ -113,33 +86,19 @@ void fmc_init(int argc, uint32_t arg1, uint32_t arg2, uint32_t arg3)
    hnand.Init.Waitfeature     = FMC_NAND_WAIT_FEATURE_ENABLE; /* Waiting enabled when communicating with the NAND */
    hnand.Init.MemoryDataWidth = FMC_NAND_MEM_BUS_WIDTH_8; /* An 8-bit NAND is used */
    hnand.Init.EccComputation  = FMC_NAND_ECC_DISABLE; /* The HAL enable ECC computation when needed, keep it disabled at initialization */
-   hnand.Init.EccAlgorithm    = BSP_NAND_ECC_ALGO; /* Hamming or BCH algorithm */
-   hnand.Init.BCHMode         = BSP_NAND_ECC_BCH_MODE; /* BCH4 or BCH8 if BCH algorithm is used */
-#if BSP_NAND_ECC_SECTOR_SIZE == 256
-   hnand.Init.EccSectorSize   = FMC_NAND_ECC_SECTOR_SIZE_256BYTE;
-#elif BSP_NAND_ECC_SECTOR_SIZE == 512
+   hnand.Init.EccAlgorithm    = FMC_NAND_ECC_ALGO_BCH; /* Hamming or BCH algorithm */
+   hnand.Init.BCHMode         = FMC_NAND_BCH_8BIT; /* BCH4 or BCH8 if BCH algorithm is used */
    hnand.Init.EccSectorSize   = FMC_NAND_ECC_SECTOR_SIZE_512BYTE; /* BCH works only with 512-byte sectors */
-#elif BSP_NAND_ECC_SECTOR_SIZE == 1024
-   hnand.Init.EccSectorSize   = FMC_NAND_ECC_SECTOR_SIZE_1024BYTE;
-#elif BSP_NAND_ECC_SECTOR_SIZE == 2048
-   hnand.Init.EccSectorSize   = FMC_NAND_ECC_SECTOR_SIZE_2048BYTE;
-#elif BSP_NAND_ECC_SECTOR_SIZE == 4096
-   hnand.Init.EccSectorSize   = FMC_NAND_ECC_SECTOR_SIZE_4096BYTE;
-#elif BSP_NAND_ECC_SECTOR_SIZE == 8192
-   hnand.Init.EccSectorSize   = FMC_NAND_ECC_SECTOR_SIZE_8192BYTE;
-#else
-#error "Unsupported ECC sector size"
-#endif
    hnand.Init.TCLRSetupTime   = 2;
    hnand.Init.TARSetupTime    = 2;
 
    /* hnand Config */
-   hnand.Config.PageSize = BSP_NAND_PAGE_SIZE;
-   hnand.Config.SpareAreaSize = BSP_NAND_SPARE_AREA_SIZE;
-   hnand.Config.BlockSize = BSP_NAND_BLOCK_SIZE;
-   hnand.Config.BlockNbr = BSP_NAND_BLOCK_NBR;
-   hnand.Config.PlaneSize = BSP_NAND_PLANE_SIZE;
-   hnand.Config.PlaneNbr = BSP_NAND_PLANE_NBR;
+   hnand.Config.PageSize = 4096;     // bytes
+   hnand.Config.SpareAreaSize = 256; // bytes
+   hnand.Config.BlockSize = 64;      // pages
+   hnand.Config.BlockNbr = 4096;     // blocks
+   hnand.Config.PlaneSize = 1024;    // blocks
+   hnand.Config.PlaneNbr = 2;        // planes
    hnand.Config.ExtraCommandEnable = ENABLE;
 
    /* ComSpaceTiming */
@@ -164,7 +123,7 @@ void fmc_init(int argc, uint32_t arg1, uint32_t arg2, uint32_t arg3)
 
    /* Initialize NAND HAL ECC computations */
    NAND_EccConfigTypeDef EccConfig = {0};
-   EccConfig.Offset = BSP_NAND_ECC_OFFSET;
+   EccConfig.Offset = 2;
    if (HAL_NAND_ECC_Init(&hnand, &EccConfig) != HAL_OK) {
       my_printf("HAL_NAND_ECC_Init() != HAL_OK\r\n");
       return;
@@ -188,9 +147,9 @@ void fmc_init(int argc, uint32_t arg1, uint32_t arg2, uint32_t arg3)
    }
 
    /* Test the NAND ID correctness */
-   if ((pNAND_ID.Maker_Id  != BSP_NAND_MANUFACTURER_CODE) || (pNAND_ID.Device_Id
-            != BSP_NAND_DEVICE_CODE) || (pNAND_ID.Third_Id != BSP_NAND_THIRD_ID)
-         || (pNAND_ID.Fourth_Id != BSP_NAND_FOURTH_ID)) {
+   if ((pNAND_ID.Maker_Id  != 0x2c) || (pNAND_ID.Device_Id
+            != 0xd3) || (pNAND_ID.Third_Id != 0x90)
+         || (pNAND_ID.Fourth_Id != 0xa6)) {
       my_printf("Unexpected ID read:\r\n");
       my_printf("maker=0x%x, dev=0x%x, 3rd=0x%x, 4th=0x%x\r\n",
             pNAND_ID.Maker_Id, pNAND_ID.Device_Id,
